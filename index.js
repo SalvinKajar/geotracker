96da633c4c2fdc06d24f070ab71b55ab53d4441a
SalvinKajar <salvinkajar000@gmail.com>
2021-12-01 16:35:11 +0530

    add start

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express= require("express");
 const app= express()
-
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use( express.static( "./views" ) );
 app.get("/",function(req,res){
-    res.send("working");
+    res.render("index");
 });
 
 app.listen(process.env.PORT || 5000);
