89525ba50cd34ab042b7222376c03129aa584bf9
SalvinKajar <salvinkajar000@gmail.com>
2021-12-01 17:20:23 +0530

    add start

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express= require("express");
+const RequestIp = require('@supercharge/request-ip');
 const app= express()
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use( express.static( "./views" ) );
 app.get("/",function(req,res){
     console.log("double working");
+    const ip = RequestIp.getClientIp(req);
+    console.log("YOur IP: ",ip);
     res.render("index");
 });
 
