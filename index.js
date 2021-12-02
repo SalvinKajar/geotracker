50f135d7bf20b960fe48f6a7082efa2d65c5a19e
SalvinKajar <salvinkajar000@gmail.com>
2021-12-02 21:29:10 +0530

    add start

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express= require("express");
 const RequestIp = require('@supercharge/request-ip');
 const app= express()
-
- 
+const ipfetch = require('ip-fetch');
+let info;
+let geoarr=[]
 
  
 
@@ -12,18 +13,23 @@ app.set("view engine", "ejs");
 app.use( express.static( "./views" ) );
 
 app.get("/",function(req,res){
+    
     var geoip = require('geoip-lite');
     console.log("double working");
     var ip = RequestIp.getClientIp(req);
     
-    var geo = geoip.lookup(ip);
-    console.log(geo);
+    let fun = async () =>{
+        info = await ipfetch.getLocationNpm(ip); 
+        console.log(info);
+        geoarr.push(info);
+    }
+    fun();
     larr.push(ip);
     
     
     console.log("YOur IP: ",larr);
     console.log(geo);
-
+    console.log("geo array:",geoarr);
     res.render("index");
 });
 
--- test.js
+++ test.js
@@ -1,6 +1,12 @@
-var geoip = require('geoip-lite');
- 
-var ip = '207.97.227.239';
-var geo = geoip.lookup(ip);
- 
-console.log(geo);
+const ipfetch = require('ip-fetch');
+
+let info;
+let arr=[]
+let fun = async () =>{
+    info = await ipfetch.getLocationNpm('103.142.198.167'); 
+    console.log(info);
+    arr.push(info);
+}
+fun();
+
+console.log(arr)
