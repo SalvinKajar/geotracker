f0acb2d54d4697c3cf106aba68faa952df2aa562
SalvinKajar <salvinkajar000@gmail.com>
2021-12-02 19:18:34 +0530

    add start

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express= require("express");
 const RequestIp = require('@supercharge/request-ip');
 const app= express()
 var geoip = require('geoip-lite');
+ 
+
+ 
+
 let larr = []
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
@@ -9,9 +13,12 @@ app.use( express.static( "./views" ) );
 
 app.get("/",function(req,res){
     console.log("double working");
-    const ip = RequestIp.getClientIp(req);
-    larr.push(ip);
+    var ip = RequestIp.getClientIp(req);
+    
     var geo = geoip.lookup(ip);
+    console.log(geo);
+    larr.push(ip);
+    
     
     console.log("YOur IP: ",larr);
     console.log(geo);
